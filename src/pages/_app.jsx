c8c28a1f23d2cb877e3f874980fc2a991b583830
David Finton <dave.finton@gmail.com>
2023-03-18 18:31:15 -0500

    added google site verification

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,14 @@
-import { StoreProvider } from '../components/store/store-provider';
+import Head from 'next/head';
+import { StoreProvider } from '@/components/store/store-provider';
 
 import '@/styles/app.scss';
 
 export default function App({ Component, pageProps }) {
   return (
     <StoreProvider {...pageProps}>
+      <Head>
+        <meta key="google-site-verification" name="google-site-verification" content="xJ93_vi3HLcGhUZSPIgmJI6OD4sDM6_zQ_fNE8BUs3g" />
+      </Head>
       <Component {...pageProps} />
     </StoreProvider>
   )
